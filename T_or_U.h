d90060540d1621f2af5903f81b62491f629975f7
Yonatan Reicher <yony252525@gmail.com>
2020-11-30 20:43:58 +0200

    Refactored T_or_U.h and added documentation

--- T_or_U.h
+++ T_or_U.h
@@ -3,19 +3,42 @@
 #define define_or(T, U)                       \
     typedef struct T##_or_##U {               \
         bool is1;                             \
-        T value1;                             \
-        U value2;                             \
+        union {                               \
+            T value1;                         \
+            U value2;                         \
+        };                                    \
     } T##_or_##U;                             \
                                               \
     T##_or_##U first_##T##_or_##U(T value) {  \
-        return (T##_or_##U){true, value};     \
+        T##_or_##U ret = {0};                 \
+        ret.is1 = true;                       \
+        ret.value1 = value;                   \
+        return ret;                           \
     }                                         \
                                               \
     T##_or_##U second_##T##_or_##U(U value) { \
         T##_or_##U ret = {0};                 \
+        ret.is1 = false;                      \
         ret.value2 = value;                   \
         return ret;                           \
     }
 
-#define is_first(T_or_U) (T_or_U.is1)
-#define is_second(T_or_U) (!(T_or_U.is1))
+/*
+does the T_or_U have a T value?
+
+Input:
+    T_or_U T_OR_U - The container
+Output: bool
+    true if"f the container has a T value
+*/
+#define is_first(T_OR_U) ((T_OR_U).is1)
+
+/*
+does the T_or_U have a U value?
+
+Input:
+    T_or_U T_OR_U - The container
+Output: bool
+    true if"f the container has a U value
+*/
+#define is_second(T_OR_U) (!(T_OR_U).is1)
